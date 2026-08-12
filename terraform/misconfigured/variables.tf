26a0852011d9cd96fa9e0a628c49e2a6fcacfeeb
Muhammad Umer <mumer1333@gmail.com>
2026-08-12 00:14:28 -0500

    Add folder structure, providers.tf, and variables.tf for misconfigured and remediated environments

---
+++ terraform/misconfigured/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "account_id" {
+  description = "Your AWS account ID"
+  type        = string
+}
